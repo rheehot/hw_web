5a5fbabf1cbd41264e8b7746ec708cd5303bec97
Seungsoo <seungsoo0701@gmail.com>
2022-01-05 00:37:10 +0900

    port 5000

--- 4주차 과제/app.py
+++ 4주차 과제/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 from pymongo import MongoClient
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://test:test@localhost', 27017)
+# client = MongoClient('localhost', 27017)
 db = client.dbhomework
 
 
@@ -41,4 +42,4 @@ def view_orders():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5003, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
